3aa81d5f9a5970d2f4e8aa105615beb934e31570
devgupta2607 <devgupta2607@gmail.com>
2022-03-02 15:25:08 +0530

    Added project files

---
+++ src/Store.js
@@ -0,0 +1,21 @@
+import React, {createContext, useEffect, useReducer} from "react";
+import Reducer from './Reducer'
+
+
+const initialState = {
+    base: "http://devil007.pythonanywhere.com",
+    query: "",
+    pages: {}
+};
+
+const Store = ({children}) => {
+    const [state, dispatch] = useReducer(Reducer, initialState);
+    return (
+        <Context.Provider value={[state, dispatch]}>
+            {children}
+        </Context.Provider>
+    )
+};
+
+export const Context = createContext(initialState);
+export default Store;
